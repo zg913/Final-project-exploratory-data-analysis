97c75b87dffebb0c204715ada7c30ed3a1221399
Zihang Gao <116197190+zg913@users.noreply.github.com>
2023-05-02 15:50:35 -0400

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -6,23 +6,23 @@ import nox
 nox.options.sessions = ["flake8", "black", "test"]
 
 
-# flakes style check pep8
 @nox.session(name="flake8")
 def flake8(session: Session) -> None:
+    # flakes style check pep8
     session.install("flake8")
     session.run("flake8", "final.py", "noxfile.py", "tests")
 
 
-# black style check
 @nox.session(name="black")
 def black(session: Session) -> None:
+    # black style check
     session.install("black")
     session.run("black", "final.py", "noxfile.py", "tests")
 
 
-# test
 @nox.session(name="test")
 def run_test(session: Session) -> None:
+    # test
     session.install("pytest")
     session.run("pip", "install", "-r", "requirements.txt")
     session.install(".")
